4662745d2cfa618f5992b6c54af649cbdfde5b9f
regitawilia <regitawilia@gmail.com>
2017-07-13 21:48:00 +0700

    baru

--- pbo/test/menuadmin.xaml.cs
+++ pbo/test/menuadmin.xaml.cs
@@ -13,27 +13,31 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data.SQLite;
 using System.Data;
+using test.repository;
+using test.model;
+using static System.Object;
+using static System.Windows.Forms.TabControl;
+
 
 namespace test
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
+    /// 
     public partial class menuadmin : Window
     {
+        private Akun akun = new Akun();
+        private IAkunRepository repo = new AkunRepository();
+        private Barang barang = new Barang();
+        private IBarangRepository repobar = new BarangRepository();
+        private List<int> listselected;
         public menuadmin()
         {
             InitializeComponent();
-        }
-
-        private void mini_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        private void quit_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
+            dataGrid.ItemsSource = repo.Get();
+            dgbarang.ItemsSource = repobar.Get();
+            listselected = new List<int>();
         }
 
         private void logout_Click(object sender, RoutedEventArgs e)
@@ -43,35 +47,144 @@ namespace test
             this.Close();
         }
 
-        private void tambah_Click(object sender, RoutedEventArgs e)
+        private void dg_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            FrameworkElement element = dataGrid.Columns[0].GetCellContent(e.Row);
+            if (element.GetType() == typeof(CheckBox))
+            {
+                if (((CheckBox)element).IsChecked == true)
+                {
+                    FrameworkElement cellEmpNo = dataGrid.Columns[1].GetCellContent(e.Row);
+                    int nomor = Convert.ToInt32(((TextBlock)cellEmpNo).Text);
+                    listselected.Add(nomor);
+                }
+            }
+        }
+
+        private void tampeg_Click(object sender, RoutedEventArgs e)
         {
             tambahpegawai tampil = new tambahpegawai();
             tampil.Show();
+            this.Close();
         }
 
-
-        string ConnectionString = @"Data Source=C:\Users\Owner\Documents\Visual Studio 2015\Projects\PSBO-Sayang\pbo\psbo.db;Version=3;";
-
-        private void lihat_Click(object sender, RoutedEventArgs e)
+        private void hapeg_Click(object sender, RoutedEventArgs e)
         {
-            SQLiteConnection sqliteCon = new SQLiteConnection(ConnectionString);
+
             try
             {
-                sqliteCon.Open();
-                SQLiteCommand createCommand = new SQLiteCommand(sqliteCon);
-                createCommand.CommandText = "SELECT * FROM [daftarpegawai]";
-
-                createCommand.ExecuteNonQuery();
-                System.Data.DataSet dst = new System.Data.DataSet();
-                System.Data.DataTable dt = new System.Data.DataTable();
-                SQLiteDataAdapter dr = new SQLiteDataAdapter(createCommand);
-                dr.Fill(dt);
-                dataGrid.ItemsSource = dt.AsDataView();
+                if (listselected.Count() > 0)
+                {
+                    int count = 0;
+                    foreach (int eno in listselected)
+                    {
+                        Akun emp = (from ep in repo.Get() where ep.IdPegawai == eno select ep).First();
+                        repo.Delete(emp);
+                        count++;
+                    }
+                    MessageBox.Show(count + " Row's Deleted");
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+            menuadmin aha = new menuadmin();
+            this.Close();
+            aha.tabControl1.SelectedIndex = 2;
+            aha.Show();
+        }
+
+
+        private void lihat_Click(object sender, RoutedEventArgs e)
+        {
+           
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        void editpegawai_Click(object sender, RoutedEventArgs e)
+        {
+            Akun akun = (Akun)dataGrid.SelectedItems[0];
+            editpegawai eb = new editproduk(edit);
+            eb.Show();
+            this.Close();
+        }
+
+        private void tambahbarang_Click(object sender, RoutedEventArgs e)
+        {
+            tambahproduk tampil = new tambahproduk();
+            tampil.Show();
+            this.Close();
+        }
+
+        private void datag_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+            FrameworkElement element = dgbarang.Columns[0].GetCellContent(e.Row);
+            if (element.GetType() == typeof(CheckBox))
+            {
+                if (((CheckBox)element).IsChecked == true)
+                {
+                    FrameworkElement cellEmpNo = dgbarang.Columns[1].GetCellContent(e.Row);
+                    int nomor = Convert.ToInt32(((TextBlock)cellEmpNo).Text);
+                    listselected.Add(nomor);
+                }
+            }
+        }
+
+        private void hapusbarang_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                if (listselected.Count() > 0)
+                {
+                    int count = 0;
+                    foreach (int eno in listselected)
+                    {
+                        Barang bar = (from hl in repobar.Get() where hl.IdBarang == eno select hl).First();
+                        repobar.Delete(bar);
+                        count++;
+                    }
+                    MessageBox.Show(count + " Row's Deleted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            menuadmin muncul = new menuadmin();
+            this.Close();
+            muncul.tabControl1.SelectedIndex = 3;
+            muncul.Show();
+        }
+
+        private void dgbarang_SelectionnChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        
+        private void editbarang_Click (object sender, RoutedEventArgs e)
+        {
+            Barang edit = (Barang)dgbarang.SelectedItems[0];
+            editproduk eb = new editproduk(edit);
+            eb.Show();
+            this.Close();
+        }
+
+        private void descbar_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
